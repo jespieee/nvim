0d271a4582b749e8388ef6f82750a396cd585907
jespieee <mhuhu@umich.edu>
2024-12-26 21:35:37 -0600

    fix obsidian paths + set conceallevel

--- init.lua
+++ init.lua
@@ -10,9 +10,17 @@ vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },   -- Add more filetypes as needed
+  callback = function()
+    vim.opt_local.conceallevel = 1
+  end,
 })
--- lua/config/plugins/obsidian.lua
+++ lua/config/plugins/obsidian.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = false,
-		ft = "markdown",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "Classes",
-					path = "~/class/",
-				},
-			},
-		},
-	}
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = false,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "class",
+          path = "~/vaults/class",
+        },
+      },
+      templates = {
+        subdir = "templates",
+      },
+    }
+  },
 }

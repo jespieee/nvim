3b5a967902e042e0f44a4c9ae89081a8c13e88cc
jespieee <mhuhu@umich.edu>
2024-12-27 20:39:39 -0600

    split vaults and create custom id creation funcs

--- lua/config/plugins/obsidian.lua
+++ lua/config/plugins/obsidian.lua
@@ -10,13 +10,38 @@ return {
     opts = {
       workspaces = {
         {
-          name = "class",
-          path = "~/vaults/class",
+          name = "cs354",
+          path = "~/vaults/class/cs354",
+        },
+        {
+          name = "cs536",
+          path = "~/vaults/class/cs536",
         },
       },
       templates = {
-        subdir = "templates",
+        subdir = "~/vaults/class/templates",
       },
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Use the title directly if provided, otherwise generate a timestamp
+        if title ~= nil and title ~= "" then
+          return title
+        end
+        return tostring(os.time())
+      end,
+
+      -- Optional, customize how note file names are generated given the ID, target directory, and title.
+      ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+      ---@return string|obsidian.Path The full path to the new note.
+      note_path_func = function(spec)
+        -- This is equivalent to the default behavior.
+        local cwd = vim.fn.getcwd()
+        local cwd_path = require("obsidian").Path:new(cwd)
+        local path = cwd_path / tostring(spec.id)
+        return path:with_suffix(".md")
+      end,
     }
   },
 }
